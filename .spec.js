ab5d1ac21ad93fb3c6f049a9d2b94327e52ebbba
minwook-shin <minwook0106@gmail.com>
2018-09-20 12:49:11 +0900

    fix unused lib

--- .spec.js
+++ .spec.js
@@ -1,6 +1,4 @@
 var assert = require('assert');
-var expect = require('chai').expect;
-var request = require('request');
 
 describe('Test suit', function () {
     it('Example test case', function () {
