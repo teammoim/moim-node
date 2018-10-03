cca71f0ef54e05def2b0bc8b9b2e6d1b5507c1a3
JungMinAn <qqqwkd98@gmail.com>
2018-10-03 23:53:20 +0900

    add timestamp

--- public/scripts/timeline.js
+++ public/scripts/timeline.js
@@ -32,6 +32,25 @@ Post.prototype.setImage = function (Image) {
 Post.prototype.pushImage = function (Image) {
   this.images.push(Image);
 }
+Post.prototype.Calc_timestamp = function(){//compare now time with post time to make timestamp
+  var date = new Date();
+  var Minute = 1000*60;//millisecond
+  var Hour = Minute*60;
+  var Day = Hour*24;
+  var Millisecond = date.getTime() - this.time.getTime();
+  var timestamp;
+  if (Millisecond<Minute){timestamp = "방금"}
+  else if (Millisecond<Hour){timestamp = parseInt(Millisecond/Minute) + "분 전"}
+  else if (Millisecond<Day){timestamp = parseInt(Millisecond/Hour) + "시간 전"}
+  else{
+    var hourmin = (this.time.getHours()<12 ? "오전 " : "오후 ") + this.time.getHours() + ":" + this.time.getMinutes();
+    var monthday = (this.time.getMonth()+1) + "월 " + this.time.getDate() + "일 ";
+    if (Millisecond<Day*2){timestamp = "어제 " + hourmin}
+    else if (this.time.getTime()>=1514732400000){timestamp = monthday + hourmin} // before 2018
+    else {timestamp = this.time.getFullYear() + "년 " + monthday}// after 2018
+  }
+  return timestamp;
+}
 var posts = [];
 function createPost(Post) {
   posts.push(Post);
@@ -70,11 +89,17 @@ function implementPost(Post) {
     var writer = document.createElement("span");
     writer.className = "writer";
     writer_right.appendChild(writer);
+    var br = document.createElement("br");
+    writer_right.appendChild(br);
 
     //in writer-right tag
     var timestamp = document.createElement("span");
     timestamp.className = "timestamp";
     writer_right.appendChild(timestamp);
+
+    var timestamp_time = document.createElement("small");
+    timestamp_time.className = "timestamp_time";
+    writer_right.appendChild(timestamp_time);
 
     var content = document.createElement("div");
     content.className = "content";
@@ -122,6 +147,8 @@ function implementPost(Post) {
 
     writer.innerText = Post.writer;//set writer's name
     text.innerText = Post.text; //set content text
+    timestamp.innerText = "@" + Post.writer + " ";
+    timestamp_time.innerText = Post.Calc_timestamp();
     //if writer image is not ready, set writer image default
     if (typeof Post.writerImage !== 'object') {
       writerImage.src = "images/default-writerImage.png";
@@ -139,12 +166,14 @@ var james = new Post();
 
 james.setWriter("james");
 james.setText("My name is james");
+james.setTime(2018, 9, 3, 4, 32, 0);
 
 
 
 var jin = new Post();
 jin.writer = "jin";
 jin.text = "my name is jin";
+jin.setTime(2018, 9, 3, 23, 46, 0);
 moim = new Image();
 moim.src = "images/logo.png";
 jin.setImage(moim);
