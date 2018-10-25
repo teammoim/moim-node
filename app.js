40f2ae8ff89fff894a70e1bd027ec435c7cb493c
herdson <devherdson@gmail.com>
2018-10-25 13:54:16 +0900

    Temp commit

--- app.js
+++ app.js
@@ -83,6 +83,26 @@ app.post('/trylogin', function(request, response) {
     response.redirect('/profile');
 });
 
+app.post('/loginprovider', (req, res) =>
+{
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    });
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {next(createError(404));});
 
