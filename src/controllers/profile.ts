46a76eb6949878d423f743a1c65963ca41df5f40
herdson <devherdson@gmail.com>
2018-10-28 18:30:01 +0900

    Fix 302 error of profile.ts

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -3,12 +3,13 @@ import * as firebase from "firebase";
 const dbconfig = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
 const timelines = firebase.database();
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
-
 
 export let index = (req: Request, res: Response) => {
     const you = firebase.auth().currentUser;
+    if (!you) {
+        console.log("You have not logged");
+        res.send();
+    }
     const yourmail = you.email;
     // const yournickname = you.displayName;
   res.render("user/profile", {
