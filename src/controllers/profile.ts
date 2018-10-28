b7520747ba6881ba08659e7d18aca02354941253
herdson <devherdson@gmail.com>
2018-10-28 18:05:35 +0900

    Fix login session, fetch profile data, and server can send timeline json data

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -1,4 +1,12 @@
 import { Request, Response } from "express";
+import * as firebase from "firebase";
+const dbconfig = require("../../fbconfig.js");
+export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
+const timelines = firebase.database();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+
 export let index = (req: Request, res: Response) => {
   res.render("user/profile", {
     title: "Home"
--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import * as firebase from "firebase";
+const dbconfig = require("../../fbconfig.js");
+export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
+const auth = firebase.auth();
+const timelines = firebase.database();
+
 export let index = (req: Request, res: Response) => {
+    // tl have json object => {body: ~~~, like: ~~~, time: ~~~, title: ~~~}
+    const tl: object[] = [];
+    timelines.ref("/timeline/").once("value", (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            const child = childSnapshot.val();
+            tl.push(child);
+        });
+        // console.log(tl);
+    });
   res.render("timeline/timeline", {
     title: "Home"
   });
