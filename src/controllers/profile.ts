f2abd098afb31c1d32f13cc8c3373cb0405520d6
herdson <devherdson@gmail.com>
2018-10-28 18:11:03 +0900

    Server can send profile data

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -8,6 +8,9 @@ provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
 
 
 export let index = (req: Request, res: Response) => {
+    const you = firebase.auth().currentUser;
+    const yourmail = you.email;
+    // const yournickname = you.displayName;
   res.render("user/profile", {
     title: "Home"
   });
