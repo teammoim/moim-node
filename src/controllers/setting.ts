776ca8d8500420ac0925cc4ec244fca321a4c841
anheew1 <anheew1@kookmin.ac.kr>
2018-11-23 01:50:48 +0900

    get setting information

--- src/controllers/setting.ts
+++ src/controllers/setting.ts
@@ -5,5 +5,12 @@ export let index = (req: Request, res: Response) => {
   });
 };
 export let setting = (req: Request, res: Response) => {
-
+  const email = req.body.email;
+  const password = req.body.password;
+  const phone = req.body.phonenumber;
+  const realname = req.body.name;
+  const nickname = req.body.nickname;
+  const birth = req.body.birthdate;
+  const gender = req.body.chk_info;
+  const intro = req.body.intro;
 };
