93fea28b987c3f08c6ba8b16c8e89bd7d1385b99
herdson <devherdson@gmail.com>
2018-11-23 13:11:27 +0900

    Apply anheew1's requirement

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import * as firebase from "firebase";
-import { log } from "async";
 const dbconfig = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
 const auth = firebase.auth();
@@ -11,39 +10,18 @@ export let index = (req: Request, res: Response) => {
         res.redirect("/login");
     }
     else {
-        // tl have json object of timline
-        const tl: object[] = [];
+        // contents have json object of timline
+        const contents: object[] = [];
         timelines.ref("/timeline/").once("value", (snapshot) => {
             snapshot.forEach((childSnapshot) => {
                 const child = childSnapshot.val();
-                tl.push(child);
+                contents.push(child);
             });
-            console.log(tl);
+            console.log(contents);
         });
     }
 
   res.render("timeline/timeline", {
     title: "Home"
-    /* to-do
-     * give client to DB JSON Data
-     * hope variable name is contents
-       contents : JSON */
   });
-};
-export let post = (req: Request, res: Response) => {
-  const text = req.body.text;
-  // const imageurls = req.body.images.split(","); // not made
-};
-export let comment = (req: Request, res: Response) => {
-  const text = req.body.text;
-  const postid = req.body.postid;
-};
-export let follow = (req: Request, res: Response) => {
-  const uid = req.body.uid;
-};
-export let like = (req: Request, res: Response) => {
-  const postid = req.body.postid;
-};
-export let goprofile = (req: Request, res: Response) => {
-  const uid = req.body.uid;
 };
