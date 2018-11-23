269bad9e0d99f5b66a86ce372173a6f133bb61c3
herdson <devherdson@gmail.com>
2018-11-23 13:14:21 +0900

    Restore anheew1's code

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -21,7 +21,52 @@ export let index = (req: Request, res: Response) => {
         });
     }
 
-  res.render("timeline/timeline", {
-    title: "Home"
-  });
-};
+    res.render("timeline/timeline", {
+       title: "Home"
+       /* to-do
+        * give client to DB JSON Data
+        * hope variable name is contents
+          contents : JSON */
+
+          /*
+          Example of contents =>
+          [ { name: 't@t.com', text: 'hello, world!!' },
+{ name: 'test@test.com', text: 'hi t@t.com!' },
+{ name: 'smart8612@gmail.com', text: '74890378920' },
+{ name: 'smart8612@gmail.com', text: '123456789' },
+{ name: 'smart8612@gmail.com', text: '안녕하세요' },
+{ name: 'smart8612@gmail.com', text: '최호경 ㅎㅇㅎㅇ' },
+{ name: 'smart8612@gmail.com', text: 'ㅂㅈㄷㄱ' },
+{ name: 'smart8612@gmail.com', text: '쇼ㅕㅑ' },
+{ name: 'smart8612@gmail.com', text: 'ㅠ오어암' },
+{ name: 'smart8612@gmail.com', text: 'ㅍ노어더ㅗㅓ' },
+{ name: 'smart8612@gmail.com', text: '안녕하세요' },
+{ name: 'smart8612@gmail.com', text: 'ifjiehisfh' },
+{ name: 'smart8612@gmail.com', text: 'qwertyy' },
+{ name: 'qwerty@qwerty.com', text: 'didjejjwj' },
+{ name: 'qwertyu@qwertyu.com', text: 'bxcxhlcchkc' } ]
+          */
+     });
+
+   };
+   export let post = (req: Request, res: Response) => {
+     const text = req.body.text;
+     // const imageurls = req.body.images.split(","); // not made
+   };
+
+   export let comment = (req: Request, res: Response) => {
+     const text = req.body.text;
+     const postid = req.body.postid;
+   };
+
+   export let follow = (req: Request, res: Response) => {
+     const uid = req.body.uid;
+   };
+
+   export let like = (req: Request, res: Response) => {
+     const postid = req.body.postid;
+   };
+
+   export let goprofile = (req: Request, res: Response) => {
+     const uid = req.body.uid;
+   };
