179244002ec4fc1a3a5d3272e524cb510c9db41f
herdson <devherdson@gmail.com>
2018-11-24 21:09:01 +0900

    [profile.ejs && profile.ts] Now can print nickname on profile page if user has nickname

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -4,18 +4,52 @@ const dbconfig = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
 const timelines = firebase.database();
 
+function dataChecker(target: string) {
+    const check = JSON.parse(target);
+    let flag = true;
+
+    if (check.birthday == "" || check.birthday == undefined) {
+        flag = false;
+    }
+    else if (check.email == "" || check.email == undefined) {
+        flag = false;
+    }
+    else if (check.gender == "" || check.gender == undefined) {
+        flag = false;
+    }
+    else if (check.name == "" || check.name == undefined) {
+        flag = false;
+    }
+    else if (check.nickname == "" || check.nickname == undefined) {
+        flag = false;
+    }
+    else if (check.phonenumber == "" || check.phonenumber == undefined) {
+        flag = false;
+    }
+    else if (check.uid == "" || check.uid == undefined) {
+        flag = false;
+    }
+    return flag;
+}
+
 export let index = (req: Request, res: Response) => {
     const curruser = firebase.auth().currentUser;
     if (!curruser) {
         console.log("you have not logged");
         res.redirect("/login");
-    }
-    else {
+    } else {
         // Valid the user have correct json data.
         timelines.ref("/users/" + curruser.uid).once("value").then((snapshot) => {
-
         console.log(snapshot.val());
 
+        if (!dataChecker(JSON.stringify(snapshot.val()))) {
+            // Some data not written
+            console.log("User infomation saved not correctly");
+            res.redirect("/changesetting");
+        } else {
+            // All data saved to DB correctly
+
+        }
         // if (!snapshot.val()) {
         //     timelines.ref("/users/" + curruser.uid).set({
         //         birthday: "Undetected",
@@ -29,12 +63,11 @@ export let index = (req: Request, res: Response) => {
         // }
     });
 
-        const currusermail = curruser.email;
-        res.render("user/profile", {
-          title: "Home",
-          email: currusermail
-          // if you want to show user information, write here
-          // and need timeline posts
-        });
+    const printingname = (curruser.displayName == "" || curruser.displayName == undefined) ? curruser.email : curruser.displayName;
+
+    res.render("user/profile", {
+      title: "Home",
+      name: printingname
+    });
     }
 };
