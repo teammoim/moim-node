f36d2ed97c4ffdbac3e320502949116ebe059002
herdson <devherdson@gmail.com>
2018-11-25 13:04:46 +0900

    [logout.ts] Add condition if user not logged, send error.

--- src/controllers/logout.ts
+++ src/controllers/logout.ts
@@ -8,11 +8,16 @@ export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : fireba
 const auth = firebase.auth();
 
 export let logout = (req: Request, res: Response) => {
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-      res.redirect("/");
-    }, function(error) {
-      // An error happened.
-      res.redirect("/");
-    });
+
+    if (auth.currentUser) {
+        auth.signOut().then(function() {
+          // Sign-out successful.
+          res.redirect("/");
+        }, function(error) {
+          // An error happened.
+          res.send("로그아웃 중 알 수 없는 오류가 발생했습니다.");
+        });
+    } else {
+        res.send("유저가 로그인하지 않았습니다.");
+    }
 };
