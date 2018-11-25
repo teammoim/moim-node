66ea72b0b9132312a1eb48764624dd1898cc8eee
herdson <devherdson@gmail.com>
2018-11-25 12:11:47 +0900

    [index.ts/ejs] Now the homepage can print user's nick name

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
+import * as firebase from "firebase";
+const dbconfig = require("../../fbconfig.js");
+export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
+const timelines = firebase.database();
+const auth = firebase.auth();
+
 export let index = (req: Request, res: Response) => {
   res.render("index/index", {
-    title: "Home"
+    title: (auth.currentUser != undefined) ? auth.currentUser.displayName : "회원님!"
   });
 };
