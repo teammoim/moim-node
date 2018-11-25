9ac153d32dd470cf86ed77809593282054b4ce54
herdson <devherdson@gmail.com>
2018-11-26 02:54:43 +0900

    [subscribe.ts/ejs] Implemented subscribe router and front page.

---
+++ src/controllers/subscribe.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import * as firebase from "firebase";
+const dbconfig = require("../../fbconfig.js");
+export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
+const timelines = firebase.database();
+const auth = firebase.auth();
+
+export let index = (req: Request, res: Response) => {
+    res.render("user/subscribe", {added: undefined, users: undefined});
+};
+
+export let finduser = (req: Request, res: Response) => {
+    if (!auth.currentUser) {
+        console.log("You have not logged");
+        res.redirect("/login");
+    }
+
+    const target = req.body.nickname;
+
+    console.log(target);
+
+    const curruser = auth.currentUser;
+    const found: object[] = [];
+
+    timelines.ref("/users/").once("value", (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            if (childSnapshot.val().nickname == target) {
+                console.log("Target found");
+                found.push(childSnapshot.val());
+            }
+        });
+        console.log(found);
+        res.render("user/subscribe", {added: undefined, users: found});
+    });
+    // example rendering code <%= name %>, inner code <% %>
+
+};
+
+export let subscribe = (req: Request, res: Response) => {
+    const addTarget = req.body.uid;
+
+    timelines.ref("/users/" + auth.currentUser.uid).once("value", (snapshot) => {
+        if (snapshot.val().follow == "" || snapshot.val().follow == undefined || snapshot.val().follow == {}) {
+            timelines.ref("/users/" + auth.currentUser.uid + "/follow").set({
+                [addTarget]: true
+            });
+        } else {
+            const dbaccess = timelines.ref("/users/" + auth.currentUser.uid + "/follow");
+            dbaccess.update({[addTarget]: true});
+        }
+
+        res.render("user/subscribe", {added: true, users: undefined});
+    });
+};
