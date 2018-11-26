08b4536753f3f4e348c2fdc51b1529b901d7b4b6
ChoiJuwon <sch2307@naver.com>
2018-11-26 14:23:49 +0900

    login.ts: Change stats parameters and Add debug mode

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,30 +1,37 @@
 import { Request, Response } from "express";
 import express from "express";
 
-// Initialize firebase
+/* Initialize firebase */
 import * as firebase from "firebase";
-const dbconfig = require("../../fbconfig.js");
+const dbconfig: any = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
 const auth = firebase.auth();
 
+const DEBUG_MODE = false;
+
 export let index = (req: Request, res: Response) => {
-  res.render("user/login", {title: "Home"});
+    res.render("user/login", {title: "Home"});
 };
+
+/**
+ * @param req
+ * @param res
+ *  1. Login Success
+ *  2. Login Failed
+ */
 
 export let login = (req: Request, res: Response) => {
     if (!!auth.currentUser) {
         res.redirect("/profile");
     }
-  const email = req.body.email;
-  const password = req.body.password;
-  auth.signInWithEmailAndPassword(email, password).then((user) => {
-      res.redirect("/profile");
-  })
-  .catch(function (error) {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode + " " + errorMessage);
-    res.send("로그인 실패! 다시 시도해주십시오.");
-});
+    const email = req.body.email;
+    const password = req.body.password;
+    auth.signInWithEmailAndPassword(email, password).then((user) => {
+        res.redirect("/profile");
+    }).catch(function (error) {
+        if (DEBUG_MODE) {
+            console.log(error.code + " " + error.message);
+        }
+        res.send(2);
+    });
 };
