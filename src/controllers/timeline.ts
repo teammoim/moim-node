f62002c5f9ede191f374c1cd6319f4be2610a1b2
anheew1 <anheew1@kookmin.ac.kr>
2018-11-27 22:43:07 +0900

    [timeline.ts] update comment

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -27,29 +27,9 @@ export let index = (req: Request, res: Response) => {
        /* to-do
         * give client to DB JSON Data
         /* client need...
-           contents : {
-             profile : [{name : "name", photourl : "" }]
-             posts : [{ uid : "", postid : "", name: "kim", text : "my name is kim", imageurl : "firstsrc,secondsrc" ,
-               comments : [{ uid : "", commentid : "" , photourl: "", name: "", text: "" },
-                           { uid : "", commentid : "" , photourl: "", name: "", text: "" },
-                           { uid : "", commentid : "" , photourl: "", name: "", text: "" }]},
-
-                      { uid : "", postid : "", name : "Joh" text : "my name is joh", imageurl : "firstsrc,second" ,
-               comments : [{ uid : "", commentid : "" , photourl: "", name: "", text: "" },
-                           { uid : "", commentid : "" , photourl: "", name: "", text: "" },
-                           { uid : "", commentid : "" , photourl: "", name: "", text: "" }]
-             }]
-             friends : [{uid : "", photourl="" , name : "zelda"},
-                        {uid : "", photourl="" , name : "link"}]
-           }
-
-          client must need all of them
-
-          photourl은 게시물 올린 사람의 프로필 이미지 - single
-          imageurl은 게시물 내부의 이미지 - 여러개의 이미지 쉼표(,)로 구분
-
-          timeline.ejs에서는 유저가 팔로우한 친구의 게시물들을 최신부터 표시
-          profile.ejs에서는 유저가 쓴 게시물들을 최신부터 표시
+          user의 JSON DATA
+          [timeline.ejs] follow한 친구들의 Post JSON DATA
+          [profile.ejs] 내가 쓴 Post JSON DATA
      */
      });
 
@@ -57,7 +37,6 @@ export let index = (req: Request, res: Response) => {
    // writing post
    export let post = (req: Request, res: Response) => {
      const text = req.body.text;
-     // const imageurls = req.body.images.split(","); // not made
    };
   // writing comment at post
    export let comment = (req: Request, res: Response) => {
