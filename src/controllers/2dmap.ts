044104ef7238fa359221801bf5c68764f0a13b98
herdson <devherdson@gmail.com>
2018-12-01 19:53:54 +0900

    Plz mergegit add .git add .

--- src/controllers/2dmap.ts
+++ src/controllers/2dmap.ts
@@ -7,10 +7,6 @@ const markers = firebase.database();
 
 export let index = (req: Request, res: Response) => {
     markers.ref("/events/").once("value").then((snapshot) => {
-        // const evx: object[] = [];
-        // snapshot.forEach((child) => {
-        //     evx.push(child.val());
-        // });
         console.log(snapshot.val());
         res.render("ar/2dmap", {
           title: "Home",
