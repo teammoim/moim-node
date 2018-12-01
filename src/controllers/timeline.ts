8230f9d4ad1d8e9b628fbcb0f3fa48ec9061ad86
anheew1 <anheew1@kookmin.ac.kr>
2018-12-02 01:45:27 +0900

    [timeline.ts] change db path correctly

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -6,10 +6,13 @@ const firebase_config = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(firebase_config) : firebase.app();
 const auth = firebase.auth();
 const firebase_db = firebase.database();
+const currentuid = auth.currentUser.uid;
 
 /* Userspace variable Initialize */
-const serverDate = new Date();
+// const serverDate = new Date(); 
+// this is wrong because it is time that server created
 const DEBUG_FLAG = true;
+
 
 export let index = (req: Request, res: Response) => {
     if (auth.currentUser) {
@@ -33,17 +36,16 @@ export let index = (req: Request, res: Response) => {
 
 export let createPost = (req: Request, res: Response) => {
     const post_text = req.body.text;
-  // const img_url = req.body.img_url;
+    const img_url = req.body.img_url;
 
 
-    const newPostKey = firebase_db.ref().child("post").push().key;
+  const newPostKey = new Date().getTime();
 
-    firebase_db.ref("/post/" + auth.currentUser.uid + "/" + newPostKey).set({
+  firebase_db.ref("/post/" + currentuid + "/" + newPostKey).set({
         postId: newPostKey,
         text: post_text,
         uid: auth.currentUser.uid,
-        timestamp: serverDate.getTime(),
-        // img_url: img_url // img_url_Array
+        url: img_url // img_url_Array
     }).catch(function(error) {
         if (!error) {
             // data submit successfully
@@ -57,10 +59,10 @@ export let createPost = (req: Request, res: Response) => {
 };
 
 export let delPost = (req: Request, res: Response) => {
-    const postId = req.body.postId;
-    firebase_db.ref("/post/" + postId).once("value").then(function(data) {
+  const postId = req.body.postId;
+  firebase_db.ref("/post/" + currentuid + "/" + postId).once("value").then(function (data) {
         if (data != undefined) {
-            firebase_db.ref("post/" + postId).remove();
+            firebase_db.ref("post/" + currentuid + "/" + postId).remove();
             // Delete Complete alert
         } else {
             console.log("Data is undefined");
@@ -78,16 +80,12 @@ export let delPost = (req: Request, res: Response) => {
 
 export let comment = (req: Request, res: Response) => {
     const postId = req.body.postid;
-    const writerUid = req.body.writerUid;
     const comments_text = req.body.text;
+    const newCommentsKey = new Date().getTime();
 
-    const newCommentsKey = firebase_db.ref("/post/" + postId).child("comments").push().key;
-
-    firebase_db.ref("/post/" + postId).child("/comments/" + newCommentsKey).set({
-        commentsId: newCommentsKey,
+  firebase_db.ref("/post/" + currentuid +"/"+postId).child("/comments/" + newCommentsKey).set({
         comments: comments_text,
-        timestamp: serverDate.getTime(),
-        writerUid: writerUid,
+        uid: currentuid,
     }).catch(function(error) {
         if (DEBUG_FLAG) {
             console.log(error.code + " , " + error.message);
@@ -98,9 +96,9 @@ export let comment = (req: Request, res: Response) => {
 export let delComments = (req: Request, res: Response) => {
     const postId = req.body.postId;
     const commentsId = req.body.commentsId;
-    firebase_db.ref("/post/" + postId).child("/comments/" + commentsId).once("value").then(function(data) {
+    firebase_db.ref("/post/" +currentuid+"/"+ postId).child("/comments/" + commentsId).once("value").then(function(data) {
         if (data != undefined) {
-            firebase_db.ref("/post/" + postId).child("/comments/" + commentsId).remove();
+            firebase_db.ref("/post/" + currentuid+"/"+postId).child("/comments/" + commentsId).remove();
         } else {
             console.log("Data is undefined");
         }
@@ -122,7 +120,7 @@ export let like = (req: Request, res: Response) => {
     const uid = req.body.uid;
     let tmp_likes = "";
 
-    firebase_db.ref("/post/" + postId).child("likes")
+    firebase_db.ref("/post/" + currentuid + "/" + postId).child("likes")
         .once("value").then(function (data) {
         const JsonData = data.val();
         if (JsonData.likes_list.length > 0) {
@@ -132,8 +130,7 @@ export let like = (req: Request, res: Response) => {
         console.log(error.code + " , " + error.message);
     });
 
-    firebase_db.ref("/post/" + postId).set({
-        postId: postId,
+    firebase_db.ref("/post/" + currentuid+ "/" + postId).set({
         likes_list: tmp_likes
     }).catch(function (error) {
         console.log(error.code + " , " + error.message);
@@ -141,14 +138,14 @@ export let like = (req: Request, res: Response) => {
     res.send(505);
 };
 
-   export let goprofile = (req: Request, res: Response) => {
-       const uid = "0TOUAVT4zXb4AFC98A3PkqZFCxi1"; // req.body.uid sample
-       firebase_db.ref("/users/" + uid).once("value").then(function (snapshot) {
-           const userData = snapshot.val();
-           res.render("user/profile", {
-               title: "Home",
-               name: userData.name,
-               uid: uid
-           });
-       });
+export let goprofile = (req: Request, res: Response) => {
+  var uid = req.body.uid;
+  firebase_db.ref("/users/" + uid).once("value").then(function (snapshot) {
+      const userData = snapshot.val();
+      res.render("user/profile", {
+        title: "Home",
+        name: userData.name,
+        uid: uid
+      });
+  });
    };
