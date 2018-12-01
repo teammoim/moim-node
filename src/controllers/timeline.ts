bf47391b7562f1ebfdabcbf84f6edbcdfa768415
ChoiJuwon <sch2307@naver.com>
2018-12-01 13:51:18 +0900

    timeline.ts : Implements createPost Function

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
+
+/* Firebase Initialized */
 import * as firebase from "firebase";
 const firebase_config = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(firebase_config) : firebase.app();
 const auth = firebase.auth();
 const firebase_db = firebase.database();
+
+/* Userspace variable Initialize */
+const serverDate = new Date();
+const DEBUG_FLAG = true;
 
 export let index = (req: Request, res: Response) => {
     if (auth.currentUser) {
@@ -22,12 +28,31 @@ export let index = (req: Request, res: Response) => {
     res.render("timeline/timeline", {
       title: "Home",
       contents: "{'number':123,'name':good}"
-        });
-    };
+    });
+};
 
-   export let post = (req: Request, res: Response) => {
-     const text = req.body.text;
-   };
+export let createPost = (req: Request, res: Response) => {
+    const post_text = req.body.text;
+    const img_url = req.body.img_url;
+
+    const newPostKey = firebase_db.ref().child("posts").push().key;
+
+    firebase_db.ref("/posts/" + newPostKey).set({
+        postId: newPostKey,
+        text: post_text,
+        uid: auth.currentUser.uid,
+        timestamp: serverDate.getTime(),
+        img_url: img_url // img_url_Array
+    }).catch(function(error) {
+        if (!error) {
+            // data submit successfully
+        } else {
+            if (DEBUG_FLAG) {
+                console.log(error.code + " , " + error.message);
+            }
+        }
+    });
+};
 
    export let comment = (req: Request, res: Response) => {
      const text = req.body.text;
