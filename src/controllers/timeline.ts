d76a42d4ee6a4a9c138addc7d91c48964cab233b
ChoiJuwon <sch2307@naver.com>
2018-12-01 13:46:33 +0900

    timeline.ts : Fixed Indentation and Remove unused Object

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -7,15 +7,12 @@ const firebase_db = firebase.database();
 
 export let index = (req: Request, res: Response) => {
     if (auth.currentUser) {
-        // contents have json object of timline
         const contents: object[] = [];
         firebase_db.ref("/posts/").once("value", (snapshot) => {
             snapshot.forEach((childSnapshot) => {
                 const child = childSnapshot.val();
                 contents.push(child);
             });
-            // contents will have many data
-            // console.log(contents);
         });
     } else {
         console.log("You have not logged");
@@ -25,54 +22,34 @@ export let index = (req: Request, res: Response) => {
     res.render("timeline/timeline", {
       title: "Home",
       contents: "{'number':123,'name':good}"
-       /* to-do
-        * give client to DB JSON Data
-        /* client need...
-          user의 JSON DATA
-          [timeline.ejs] follow한 친구들의 Post JSON DATA
-          [profile.ejs] 내가 쓴 Post JSON DATA
-     */
-     });
+        });
+    };
 
-};
-   // writing post
    export let post = (req: Request, res: Response) => {
      const text = req.body.text;
    };
-  // writing comment at post
+
    export let comment = (req: Request, res: Response) => {
      const text = req.body.text;
      const postid = req.body.postid;
    };
- // follow button click
+
    export let follow = (req: Request, res: Response) => {
      const uid = req.body.uid;
    };
- // like button click
+
    export let like = (req: Request, res: Response) => {
      const postid = req.body.postid;
    };
- // go profile page of clicked user
+
    export let goprofile = (req: Request, res: Response) => {
-     const uid = "0TOUAVT4zXb4AFC98A3PkqZFCxi1"; // req.body.uid sample
-     const isfollow = "true";
-     firebase_db.ref("/users/" + uid).once("value").then(function (snapshot) {
-       const userData = snapshot.val();
-       const tmp_name = userData.name;
-
-       res.render("user/profile", {
-         title: "Home",
-         name: tmp_name,
-         isfollow: "true", // isfollow = "true","false","me"
-         uid: uid
+       const uid = "0TOUAVT4zXb4AFC98A3PkqZFCxi1"; // req.body.uid sample
+       firebase_db.ref("/users/" + uid).once("value").then(function (snapshot) {
+           const userData = snapshot.val();
+           res.render("user/profile", {
+               title: "Home",
+               name: userData.name,
+               uid: uid
+           });
        });
-     });
-};
-export let editpost = (req: Request, res: Response) => {
-  const postid = req.body.postid;
-  const text = req.body.text;
-};
-export let deletepost = (req: Request, res: Response) => {
-  const postid = req.body.postid;
-};
-
+   };
