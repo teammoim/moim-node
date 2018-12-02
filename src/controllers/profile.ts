eda03111c59b4441686b96e384b7a12d639dfd78
anheew1 <anheew1@kookmin.ac.kr>
2018-12-02 14:59:42 +0900

    [profile.ts] send profile timeline

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as firebase from "firebase";
+import { Promise } from "bluebird";
 
 const dbconfig = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
@@ -40,50 +41,87 @@ export let index = (req: Request, res: Response) => {
           const name = userData.name;
           const subs = userData.subscribe;
           const subsinfo: object[] = [];
-          const postinfo: object[] = [];
+          let postinfo: any;
 
           timelines.ref("/post/" + curruser.uid).once("value").then((postshot) => {
-              postinfo.push(postshot.val());
-          });
-
-          if (subs !== undefined) {
-              Object.keys(subs).forEach((k) => {
-                  timelines.ref("/users/" + k).once("value", (snapinfo) => {
-                      // console.log(snapinfo.val());
-                      subsinfo.push(snapinfo.val());
-                      console.log(subsinfo);
-                      if (detectCallback(subsinfo, Object.keys(subs)) && detectCallback(postinfo, [])) {
-                          // console.log(postinfo);
-                          res.render("user/profile", {
-                            title: "Home",
-                            name: name,
-                            isfollow: "me", // "true","false","me"
-                            uid : "", // not need uid
-                            subscribes: subsinfo,
-                            you: userData,
-                            youpost: postinfo
-                          });
-                      }
-                  });
+            postinfo = postshot.val();
+            if (postinfo === null) {
+              throw ("there is no post on there");
+            }
+          }).catch((error) => {
+            console.log(error);
+            res.render("user/profile", {
+              title: "Home",
+              name: name,
+              isfollow: "me", // "true","false","me"
+              uid: "", // not need uid
+              subscribes: subsinfo,
+              you: userData,
+              youpost: "",
+            });
+            }).then(() => {
+            const promises: any[] = [];
+            Object.keys(postinfo).forEach((k) => {
+              const targetuid = postinfo[k].uid;
+              promises.push(new Promise((resolve) => {
+                timelines.ref("/users/" + targetuid).once("value").then((usershot) => {
+                  const userinfo = usershot.val();
+                  postinfo[k].name = userinfo["name"];
+                  postinfo[k].photourl = userinfo["url"];
+                  resolve(postinfo[k]);
+                });
+              }));
+            });
+            Promise.all(promises).then(() => {
+              console.log(postinfo);
+              res.render("user/profile", {
+                title: "Home",
+                name: name,
+                isfollow: "me", // "true","false","me"
+                uid: "", // not need uid
+                subscribes: subsinfo,
+                you: userData,
+                youpost: JSON.stringify(postinfo),
               });
-          } else {
-              if (detectCallback(postinfo, [])) {
-                  // console.log(postinfo);
-                  res.render("user/profile", {
-                    title: "Home",
-                    name: name,
-                    isfollow: "me", // "true","false","me"
-                    uid : "", // not need uid
-                    subscribes: [],
-                    you: userData,
-                    youpost: postinfo
-                  });
-              }
+            });
+          });
+            /* if (subs !== undefined) {
+                Object.keys(subs).forEach((k) => {
+                    timelines.ref("/users/" + k).once("value", (snapinfo) => {
+                        // console.log(snapinfo.val());
+                        subsinfo.push(snapinfo.val());
+                        console.log(subsinfo);
+                        if (detectCallback(subsinfo, Object.keys(subs)) && detectCallback(postinfo, [])) {
+                            // console.log(postinfo);
+                            res.render("user/profile", {
+                              title: "Home",
+                              name: name,
+                              isfollow: "me", // "true","false","me"
+                              uid : "", // not need uid
+                              subscribes: subsinfo,
+                              you: userData,
+                              youpost: postinfo
+                            });
+                        }
+                    });
+                });
+            } else {
+                if (detectCallback(postinfo, [])) {
+                    // console.log(postinfo);
+                    res.render("user/profile", {
+                      title: "Home",
+                      name: name,
+                      isfollow: "me", // "true","false","me"
+                      uid : "", // not need uid
+                      subscribes: [],
+                      you: userData,
+                      youpost: postinfo
+                    });
+                }
+            }*/
           }
-
-
-        }
-    }).catch((error) => {
+        }).catch((error) => {
+          console.log("What is that!");
     console.log(error);
 });
     }
