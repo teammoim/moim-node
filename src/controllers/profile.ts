13a32b35f1c70d84d5db23183e0e32b01da89ed0
anheew1 <anheew1@kookmin.ac.kr>
2018-12-03 23:21:27 +0900

    [profile.ts] send Comments Data to client

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as firebase from "firebase";
 import { Promise } from "bluebird";
+import { comment } from "./timeline";
 
 const dbconfig = require("../../fbconfig.js");
 export default !firebase.apps.length ? firebase.initializeApp(dbconfig) : firebase.app();
@@ -20,6 +21,132 @@ function dataChecker(target: string) {
 function detectCallback(target: object[], comparison: string[]) {
     return target.length >= comparison.length;
 }
+function sendDataFromUid(req: Request, res: Response, uid: String) {
+  timelines.ref("/users/" + uid).once("value").then((snapshot) => {
+    const userData = snapshot.val();
+    console.log(userData);
+    if (!dataChecker(JSON.stringify(snapshot.val()))) {
+      // Some data not written
+      console.log("User infomation saved not correctly");
+      console.log(snapshot.val());
+      res.redirect("/setting");
+    } else {
+      // All data saved to DB correctly
+      const name = userData.name;
+      const subs = userData.subscribe;
+      const currentuid = auth.currentUser.uid;
+      const subsinfo: object[] = [];
+      let postinfo: any;
+
+      timelines.ref("/post/" + uid).once("value").then((postshot) => {
+        postinfo = postshot.val();
+        if (postinfo === null) {
+          throw ("there is no post on there");
+        }
+      }).catch((error) => {
+        console.log(error);
+        res.render("user/profile", {
+          title: "Home",
+          name: name,
+          isfollow: "me", // "true","false","me"
+          uid: "", // not need uid
+          subscribes: subsinfo,
+          you: userData,
+          youpost: "",
+        });
+      }).then(() => {
+        const promises: any[] = [];
+        Object.keys(postinfo).forEach((k) => {
+          const targetuid = postinfo[k].uid;
+          promises.push(new Promise((resolve) => {
+            timelines.ref("/users/" + targetuid).once("value").then((usershot) => {
+              const userinfo = usershot.val();
+              postinfo[k].name = userinfo["name"];
+              postinfo[k].photourl = userinfo["url"];
+
+            }).catch((err) => {
+              console.log(err);
+            }).then(() => {
+              const commentsinfo = postinfo[k].comments;
+              if (commentsinfo !== undefined) {
+                const subPromises: any[] = [];
+                Object.keys(commentsinfo).forEach((ckey) => {
+                  const commentsuid = commentsinfo[ckey].uid;
+                  subPromises.push(new Promise((resolve) => {
+                    timelines.ref("/users/" + commentsuid).once("value").then((commentshot) => {
+                      const comuserinfo = commentshot.val();
+                      commentsinfo[ckey].name = comuserinfo["name"];
+                      commentsinfo[ckey].photourl = comuserinfo["url"];
+                      resolve(commentsinfo[ckey]);
+                    }).catch((error) => {
+                      console.log(error);
+                    });
+                  }));
+                });
+                Promise.all(subPromises).then(() => {
+                  resolve(postinfo[k]);
+                });
+              } else {
+                resolve(postinfo[k]);
+              }
+            });
+          }));
+        });
+        Promise.all(promises).then(() => {
+          console.log(postinfo);
+          for (const key in postinfo) {
+            console.log(postinfo[key].comments);
+          }
+          res.render("user/profile", {
+            title: "Home",
+            name: name,
+            isfollow: "me", // "true","false","me"
+            uid: currentuid, // not need uid
+            subscribes: subsinfo,
+            you: userData,
+            youpost: postinfo,
+          });
+        });
+      });
+      /* if (subs !== undefined) {
+          Object.keys(subs).forEach((k) => {
+              timelines.ref("/users/" + k).once("value", (snapinfo) => {
+                  // console.log(snapinfo.val());
+                  subsinfo.push(snapinfo.val());
+                  console.log(subsinfo);
+                  if (detectCallback(subsinfo, Object.keys(subs)) && detectCallback(postinfo, [])) {
+                      // console.log(postinfo);
+                      res.render("user/profile", {
+                        title: "Home",
+                        name: name,
+                        isfollow: "me", // "true","false","me"
+                        uid : "", // not need uid
+                        subscribes: subsinfo,
+                        you: userData,
+                        youpost: postinfo
+                      });
+                  }
+              });
+          });
+      } else {
+          if (detectCallback(postinfo, [])) {
+              // console.log(postinfo);
+              res.render("user/profile", {
+                title: "Home",
+                name: name,
+                isfollow: "me", // "true","false","me"
+                uid : "", // not need uid
+                subscribes: [],
+                you: userData,
+                youpost: postinfo
+              });
+          }
+      }*/
+    }
+  }).catch((error) => {
+    console.log(error);
+  });
+}
 
 export let index = (req: Request, res: Response) => {
     const curruser = auth.currentUser;
@@ -27,102 +154,6 @@ export let index = (req: Request, res: Response) => {
         console.log("you have not logged");
         res.redirect("/login");
     } else {
-      // Valid the user have correct json data.
-        timelines.ref("/users/" + curruser.uid).once("value").then((snapshot) => {
-        const userData = snapshot.val();
-        console.log(userData);
-        if (!dataChecker(JSON.stringify(snapshot.val()))) {
-            // Some data not written
-            console.log("User infomation saved not correctly");
-            console.log(snapshot.val());
-            res.redirect("/setting");
-        } else {
-            // All data saved to DB correctly
-          const name = userData.name;
-          const subs = userData.subscribe;
-          const subsinfo: object[] = [];
-          let postinfo: any;
-
-          timelines.ref("/post/" + curruser.uid).once("value").then((postshot) => {
-            postinfo = postshot.val();
-            if (postinfo === null) {
-              throw ("there is no post on there");
-            }
-          }).catch((error) => {
-            console.log(error);
-            res.render("user/profile", {
-              title: "Home",
-              name: name,
-              isfollow: "me", // "true","false","me"
-              uid: "", // not need uid
-              subscribes: subsinfo,
-              you: userData,
-              youpost: "",
-            });
-            }).then(() => {
-            const promises: any[] = [];
-            Object.keys(postinfo).forEach((k) => {
-              const targetuid = postinfo[k].uid;
-              promises.push(new Promise((resolve) => {
-                timelines.ref("/users/" + targetuid).once("value").then((usershot) => {
-                  const userinfo = usershot.val();
-                  postinfo[k].name = userinfo["name"];
-                  postinfo[k].photourl = userinfo["url"];
-                  resolve(postinfo[k]);
-                });
-              }));
-            });
-            Promise.all(promises).then(() => {
-              console.log(postinfo);
-              res.render("user/profile", {
-                title: "Home",
-                name: name,
-                isfollow: "me", // "true","false","me"
-                uid: "", // not need uid
-                subscribes: subsinfo,
-                you: userData,
-                youpost: postinfo,
-              });
-            });
-          });
-            /* if (subs !== undefined) {
-                Object.keys(subs).forEach((k) => {
-                    timelines.ref("/users/" + k).once("value", (snapinfo) => {
-                        // console.log(snapinfo.val());
-                        subsinfo.push(snapinfo.val());
-                        console.log(subsinfo);
-                        if (detectCallback(subsinfo, Object.keys(subs)) && detectCallback(postinfo, [])) {
-                            // console.log(postinfo);
-                            res.render("user/profile", {
-                              title: "Home",
-                              name: name,
-                              isfollow: "me", // "true","false","me"
-                              uid : "", // not need uid
-                              subscribes: subsinfo,
-                              you: userData,
-                              youpost: postinfo
-                            });
-                        }
-                    });
-                });
-            } else {
-                if (detectCallback(postinfo, [])) {
-                    // console.log(postinfo);
-                    res.render("user/profile", {
-                      title: "Home",
-                      name: name,
-                      isfollow: "me", // "true","false","me"
-                      uid : "", // not need uid
-                      subscribes: [],
-                      you: userData,
-                      youpost: postinfo
-                    });
-                }
-            }*/
-          }
-        }).catch((error) => {
-          console.log("What is that!");
-    console.log(error);
-});
+      sendDataFromUid(req , res , curruser.uid);
     }
 };
